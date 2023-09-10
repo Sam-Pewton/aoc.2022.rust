0d256090cf3f719e3eb20ede0f49058fceb5bd43
Sam-Pewton <s.pewton@outlook.com>
2023-09-10 14:48:26 +0100

    Updated documentation

--- day01/calorie_counting/src/main.rs
+++ day01/calorie_counting/src/main.rs
@@ -1,20 +1,36 @@
-// 
+/// 
+/// AOC 2022 Day 1
+///
 use std::fs;
 
+/// 
+/// Elf struct representing the total calories carried by an individual elf.
+///
 struct Elf {
     id: usize,
     calories: u64,
 }
 
+/// 
+/// Entrypoint
+/// 
 fn main() {
     let file = fs::read_to_string("data.txt");
     let mut elves = generate_elves(&file.unwrap());
     let most = find_most_calories(&mut elves);
     println!("Most calories: {}", most);
-    let total = find_top_three_calories(&mut elves, 3);
+    let total = find_top_n_calories(&mut elves, 3);
     println!("Top 3 sum: {}", total);
 }
 
+/// 
+/// Generate a vector of elves using the text data supplied.
+///
+/// Each elfs inventory is separated by 2 newline characters in succession, and each item in the
+/// inventory separated by 1.
+/// Each elf inventory is iterated over, parsing each item to an integer, and summing the total.
+/// This data is then used to genetare a new elf and is then pushed to the elves vector.
+///
 fn generate_elves(data: &str) -> Vec<Elf> {
     let split_data = data.split("\n\n");
     let mut elves: Vec<Elf> = vec![];
@@ -32,11 +48,20 @@ fn generate_elves(data: &str) -> Vec<Elf> {
     elves
 }
 
+/// 
+/// Find the elf carrying the most amount of calories.
+///
 fn find_most_calories(elves: &Vec<Elf>) -> u64 {
     elves.iter().fold(0, |x, y| x.max(y.calories))
 }
 
-fn find_top_three_calories(elves: &mut Vec<Elf>, top_count: usize) -> u64 {
+/// 
+/// Find the cumulative sum of calories carried by the top n number of elves.
+///
+/// The elves are first sorted by their individual calorie totals, and the elves are split by the
+/// amount that we need to sum. From here, the calories are then summed for the top n elves.
+///
+fn find_top_n_calories(elves: &mut Vec<Elf>, top_count: usize) -> u64 {
     elves.sort_by(|a, b| a.calories.partial_cmp(&b.calories).unwrap());
     elves.split_at(elves.iter().count() - top_count).1.iter().map(|x| x.calories).sum()
 }
--- day02/rock_paper_scissors/src/main.rs
+++ day02/rock_paper_scissors/src/main.rs
@@ -1,5 +1,13 @@
+/// 
+/// AOC 2022 Day 2
+///
 use std::fs;
 
+/// 
+/// Outcome enum representing the outcome state of a game.
+///
+/// The corresponding points associated with the outcome are set behind the state.
+///
 #[repr(i8)]
 enum Outcome {
     WIN = 6,
@@ -7,6 +15,11 @@ enum Outcome {
     DRAW = 3,
 }
 
+/// 
+/// Shape enum representing the hand shapes used in the game.
+///
+/// The corresponding points associated with the shape are set behind the state.
+///
 #[repr(i8)]
 #[derive(Copy, Clone)]
 enum Shape {
@@ -15,12 +28,20 @@ enum Shape {
     Scissors = 3,
 }
 
+/// 
+/// Game struct holding the game state. The game state is the shape that each of the players has
+/// chosen.
+///
 struct Game {
     user: Shape,
     opponent: Shape,
 }
 
 impl Game {
+    /// 
+    /// The first part of the day task. Used for calculating the total score when X, Y, and Z
+    /// correlate to Shapes.
+    ///
     fn new1(data: &str) -> Self {
         let mut split_data = data.split(' ');
         let lhs = match split_data.next() {
@@ -40,6 +61,10 @@ impl Game {
         Game { user: rhs, opponent: lhs }
     }
 
+    /// 
+    /// The second part of the day task. Used for calculating the total score when X, Y, and Z
+    /// correlate to the Outcome required for a game.
+    ///
     fn new2(data: &str) -> Self {
         let mut split_data = data.split(' ');
 
@@ -78,6 +103,13 @@ impl Game {
         Game { user: user_input, opponent: lhs }
     }
 
+    /// 
+    /// Calculate the number of points to award the outcome of a game. The outcome is determined by
+    /// comparing the point differences in each of the players shape.
+    ///
+    /// When the difference is 0, the outcome is a draw.
+    /// When the difference is 1 or -2, the outcome is a win for the user.
+    /// When the difference is anything else, the outcome is a loss.
     fn outcome(&self) -> Outcome {
         match self.user as i8 - self.opponent as i8 {
             0 => Outcome::DRAW,
@@ -87,16 +119,27 @@ impl Game {
     }
 }
 
+/// 
+/// Entrypoint
+///
 fn main() {
     let data = fs::read_to_string("data.txt").unwrap();
     println!("Part 1 Score: {}", calculate_score1(&data));
     println!("Part 2 Score: {}", calculate_score2(&data));
 }
 
+/// 
+/// Calculate the overall score across any number of games separated by a new line character in the
+/// data.
+///
 fn calculate_score1(data: &str) -> u64 {
     data.lines().map(|x| { let y = Game::new1(&x); y.outcome() as u64 + y.user as u64 }).sum()
 }
 
+/// 
+/// Calculate the overall score across any number of games separated by a new line character in the
+/// data.
+///
 fn calculate_score2(data: &str) -> u64 {
     data.lines().map(|x| { let y = Game::new2(&x); y.outcome() as u64 + y.user as u64 }).sum()
 }
